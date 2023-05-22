aecf231d2a4e7cfe0834ad3bf9ed254ea8310298
AleksanderOrlovIT <alexanderorlovkh@gmail.com>
2023-05-22 15:05:23 +0300

    Added exception handling for 404 and 500.

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,11 +23,20 @@ public class UserResource {
 
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable Integer id){
-        return service.findOne(id);
+        User user = service.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id" + id);
+        return user;
     }
 
     @PostMapping("/users")
-    public User createUser(@RequestBody User user){
-        return service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
